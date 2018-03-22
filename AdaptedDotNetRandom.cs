c1c2e88b125b904eeef72361e6ff201126dba83d
Emre Solak <s.emresolak@dreamharvesters.com>
2018-03-22 11:47:09 +0300

    folder restructuring namespace changed

---
+++ AdaptedDotNetRandom.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace dh.randomisation
+{
+    public class AdaptedDotNetRandom : ICreateRandom
+    {
+        private Random random;
+        private int seed;
+        
+        public AdaptedDotNetRandom(int seed)
+        {
+            random = new Random(seed);
+            this.seed = seed;
+        }
+
+        public int Seed
+        {
+            get { return seed; }
+            set
+            {
+                this.seed = value;
+                random = new Random(seed);
+            }
+        }
+
+        public float Next()
+        {
+            return (float)random.NextDouble();
+        }
+
+        public float Next(float max)
+        {
+            return Next() * max;
+        }
+
+        public float Next(float min, float max)
+        {
+            return (Next() * max) + min;
+        }
+
+        public int Next(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+
+        public int Next(int max)
+        {
+            return random.Next(0, max);
+        }
+    }
+}
---
+++ AdaptedUnityRandom.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace dh.randomisation
+{
+    public class AdaptedUnityRandom : ICreateRandom
+    {
+        private static int randCount = 0;
+
+        private static void log()
+        {
+//            randCount++;
+//            Debug.Log("random --- " + randCount);
+        }
+        
+        public AdaptedUnityRandom()
+        {
+        }
+
+        public AdaptedUnityRandom(int seed)
+        {
+            this.Seed = seed;
+        }
+
+        public int Seed
+        {
+            get { return Random.seed; }
+            set { Random.InitState(value); }
+        }
+        
+        public float Next()
+        {
+            log();
+            return Random.value;
+        }
+
+        public float Next(float max)
+        {
+            log();
+            return Random.value * max;
+        }
+
+        public float Next(float min, float max)
+        {
+            log();
+            return Random.Range(min, max);
+        }
+
+        public int Next(int min, int max)
+        {
+            log();
+            return Random.Range(min, max);
+        }
+
+        public int Next(int max)
+        {
+            log();
+            return Random.Range(0, max);
+        }
+    }
+}
